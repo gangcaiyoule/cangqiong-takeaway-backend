7e3512f99670101819f9f3e631d678e39dc493e9
SiCheng Zhang <964389211@qq.com>
2025-10-30 15:26:07 +0800

    feat: wechat payment

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -12,4 +12,17 @@ public interface OrderMapper {
      */
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void insert(Orders orders);
+
+    /**
+     * 根据订单号查询订单
+     * @param orderNumber
+     */
+    @Select("select * from orders where number = #{orderNumber}")
+    Orders getByNumber(String orderNumber);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
 }
--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -15,5 +15,17 @@ public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenId(String openid);
 
+    /**
+     * 插入数据
+     * @param user
+     */
     void insert(User user);
+
+    /**
+     * 通过userId获取用户信息
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{userId}")
+    User getById(Long userId);
 }
